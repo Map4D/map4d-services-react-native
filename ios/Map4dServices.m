c10c0533bebac4aa0ffa504a077f62fe0539f532
Huy Dang <caohuy@live.com>
2022-01-27 15:30:24 +0700

    Add react native interface

--- ios/Map4dServices.m
+++ ios/Map4dServices.m
@@ -17,61 +17,70 @@ RCT_REMAP_METHOD(multiply,
   resolve(MFServices.version);
 }
 
+/* Place | Suggestions */
 RCT_EXPORT_METHOD(fetchSuggestion:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildSuggestionsRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Place | Detail */
 RCT_EXPORT_METHOD(fetchPlaceDetail:(NSString *)placeId
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildPlaceDetailRequestWithId:placeId] resolve:resolve reject:reject];
 }
 
+/* Place | Text search */
 RCT_EXPORT_METHOD(fetchTextSearch:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildTextSearchRequestWithData:params] resolve:resolve reject:reject];
 }
 
-
+/* Place | Nearby search */
 RCT_EXPORT_METHOD(fetchNearbySearch:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildNearbySearchRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Place | Viewbox search */
 RCT_EXPORT_METHOD(fetchViewboxSearch:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildViewboxSearchRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Place | Geocode */
 RCT_EXPORT_METHOD(fetchGeocode:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildGeocodingRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Route | Directions */
 RCT_EXPORT_METHOD(fetchDirections:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildDirectionsRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Route | ETA */
 RCT_EXPORT_METHOD(fetchRouteETA:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildRouteETARequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Route | Distance matrix */
 RCT_EXPORT_METHOD(fetchDistanceMatrix:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
   [SClient fireRequest:[SRequest buildDistanceMatrixRequestWithData:params] resolve:resolve reject:reject];
 }
 
+/* Route | Graph route */
 RCT_EXPORT_METHOD(fetchGraphRoute:(id)params
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject) {
--- src/index.tsx
+++ src/index.tsx
@@ -20,3 +20,160 @@ const Map4dServices = NativeModules.Map4dServices
 export function multiply(a: number, b: number): Promise<number> {
   return Map4dServices.multiply(a, b);
 }
+
+export type MFLocationComponent = {
+  latitude: number,
+  longitude: number,
+  alias?: string,
+}
+
+export type MFViewboxComponent = {
+  southwest: MFLocationComponent,
+  northeast: MFLocationComponent,
+}
+
+export enum MFTravelMode {
+  car = 'car',
+  bike = 'bike',
+  foot = 'foot',
+  motorcycle = 'motorcycle',
+}
+
+export enum MFRouteWeighting {
+  shortest = 'shortest',
+  fastest = 'fastest',
+  balance = 'balance',
+}
+
+export enum MFLanguageResult {
+  en = 'en',
+  vi = 'vi',
+}
+
+export enum MFRouteType {
+  motorway = 'motorway',
+  trunk = 'trunk',
+  ferry = 'ferry',
+  bridge = 'bridge',
+  tunnel = 'tunnel',
+}
+
+export type MFRouteRestriction = {
+  location?: MFLocationComponent,
+  radius?: number,
+  viewbox?: MFViewboxComponent,
+  path?: MFLocationComponent[],
+  types?: MFRouteType[] | string[]
+}
+
+export type MFSuggestionParams = {
+  text: string,
+  location?: MFLocationComponent,
+  acronym?: boolean,
+}
+
+export type MFTextSearchParams = {
+  text: string,
+  types?: string[],
+  datetime?: Date,
+  location?: MFLocationComponent,
+}
+
+export type MFNearbySearchParams = {
+  location: MFLocationComponent,
+  radius: number,
+  text?: string,
+  types?: string[],
+  tags?: string[],
+  datetime?: Date,
+}
+
+export type MFViewboxSearchParams = {
+  viewbox: MFViewboxComponent,
+  text?: string,
+  types?: string[],
+  tags?: string[],
+  datetime?: Date,
+}
+
+export type MFGeocodeParams = {
+  location?: MFLocationComponent,
+  address?: string,
+  viewbox?: MFViewboxComponent,
+}
+
+export type MFDirectionsParams = {
+  origin: MFLocationComponent,
+  destination: MFLocationComponent,
+  waypoints?: MFLocationComponent[],
+  mode?: MFTravelMode | string,
+  weighting?: MFRouteWeighting | string,
+  language?: MFLanguageResult | string,
+  restriction?: MFRouteRestriction,
+}
+
+export type MFRouteETAParams = {
+  origins: MFLocationComponent[],
+  destination: MFLocationComponent,
+  mode?: MFTravelMode | string,
+  weighting?: MFRouteWeighting | string,
+  language?: MFLanguageResult | string,
+  restriction?: MFRouteRestriction,
+}
+
+export type MFDistanceMatrixParams = {
+  origins: MFLocationComponent[],
+  destinations: MFLocationComponent[],
+  mode?: MFTravelMode | string,
+  weighting?: MFRouteWeighting | string,
+  language?: MFLanguageResult | string,
+  restriction?: MFRouteRestriction,
+}
+
+export type MFGraphRouteParams = {
+  locations: MFLocationComponent[],
+  mode?: MFTravelMode | string,
+  weighting?: MFRouteWeighting | string,
+  language?: MFLanguageResult | string,
+  restriction?: MFRouteRestriction,
+}
+
+export function fetchSuggestion(params: MFSuggestionParams): Promise<object> {
+  return Map4dServices.fetchSuggestion(params);
+}
+
+export function fetchPlaceDetail(placeId: string): Promise<object> {
+  return Map4dServices.fetchPlaceDetail(placeId);
+}
+
+export function fetchTextSearch(params: MFTextSearchParams): Promise<object> {
+  return Map4dServices.fetchTextSearch(params);
+}
+
+export function fetchNearbySearch(params: MFNearbySearchParams): Promise<object> {
+  return Map4dServices.fetchNearbySearch(params);
+}
+
+export function fetchViewboxSearch(params: MFViewboxSearchParams): Promise<object> {
+  return Map4dServices.fetchViewboxSearch(params);
+}
+
+export function fetchGeocode(params: MFGeocodeParams): Promise<object> {
+  return Map4dServices.fetchGeocode(params);
+}
+
+export function fetchDirections(params: MFDirectionsParams): Promise<object> {
+  return Map4dServices.fetchDirections(params);
+}
+
+export function fetchRouteETA(params: MFRouteETAParams): Promise<object> {
+  return Map4dServices.fetchRouteETA(params);
+}
+
+export function fetchDistanceMatrix(params: MFDistanceMatrixParams): Promise<object> {
+  return Map4dServices.fetchDistanceMatrix(params);
+}
+
+export function fetchGraphRoute(params: MFGraphRouteParams): Promise<object> {
+  return Map4dServices.fetchGraphRoute(params);
+}
